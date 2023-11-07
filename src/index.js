290929875829f50072f447ace23d47a5da9ced43
Meagan <MeaganTruong1@yahoo.com>
2023-11-07 17:20:51 -0600

    meagan / nickW lab

--- src/index.js
+++ src/index.js
@@ -1,53 +1,56 @@
-// C1: For each movie returned from http://localhost:3000/movies create an image and add it to the movie-list nav element.
-// C3: when you click on each movie image in the top nav/ you should see the detail area
-// global scope to load in everywhere
-const title = document.getElementById('title')
-const year = document.getElementById('year-released')
-const des = document.getElementById('description')
-const poster = document.getElementById('detail-image')
-const watchedButton = document.getElementById('watched')
-const bloodAmt = document.getElementById('blood-amount')
-const bloodForm = document.getElementById('blood-form')
-
-
-
-document.addEventListener("DOMContentLoaded", (e) => {
+// For each movie returned from http://localhost:3000/movies create an image and add it to the movie-list nav element.
+// document.addEventListener("DOMContentLoaded", (e) => {
+    const poster = document.getElementById('detail-image')
+    const title = document.getElementById('title')
+    const year = document.getElementById('year-released')
+    const des = document.getElementById('description')
+    const watchedButton = document.getElementById('watched')
+    const bloodAmt = document.getElementById('amount')
+    const bloodForm = document.getElementById('blood-form')
+    
+    const movieList = document.getElementById('movie-list')
     const movieUrl = "http://localhost:3000/movies";
-    let movieList = document.getElementById('movie-list')
-    const showDetails = () => {
-
-
-
+    
+    const showDetails = (movie) => {
+    // let movie = movie[0]
+    title.textContent = movie.title
+    year.textContent = movie.release_year
+    des.textContent = movie.description
+    poster.src = movie.image
+    bloodAmt.textContent = movie.blood_amount
+    
+    if(movie.watched === true) {
+        watchedButton.textContent = 'Watched'
+    } else {
+       watchedButton.textContent = 'Unwatched'
+    }
+    }
+    
     fetch(movieUrl) 
-        .then(resp => resp.json())
-        .then((movies) => {
-            movies.forEach((movie) => {
-                // create HTML element
-                let movieImage = document.createElement("img");
-                // set the property / set the text
-                movieImage.src = movie.image;
-                // put it on the page
-                movieList.append(movieImage)
-                movieImage.addEventListener('click', () => {
-                    console.log(movie.title)
-                })
+    .then(res => res.json())
+    .then((movies) => {
+        movies.forEach((movie) => {
+            let movieImage = document.createElement("img");
+            movieImage.src = movie.image;
+            movieList.append(movieImage);
+            movieImage.addEventListener('click', () => {
+                showDetails(movie)
             })
-            // C2: As soon as the page loads, we should see the details of the first movie in the dataset.
-            let firstMovie = movies[0]
-            title.textContent = firstMovie.title
-            year.textContent = firstMovie.release_year
-            des.textContent = firstMovie.description
-            poster.src = firstMovie.image
-            bloodAmt.textContent = firstMovie.bloodAmt
-
-
-            // use if/else statement to DECIDE what button should say
-            if(firstMovie.watched === true){
-                watchedButton.textContent = 'Watched'
-            }else {
-                watchedButton.textContent = 'Unwatched'
-            }
-        })}
-        showDetails()
-})
-
+            // console.log(movieDetail)
+        })
+        showDetails(movies[0])
+    })
+    watchedButton.addEventListener('click', () => {
+        if( watchedButton.textContent === "watched"){
+            watchedButton.textContent = "unwatched"
+        } else {
+            watchedButton.textContent = "watched"
+        }
+    })
+    bloodForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let newDrops = e.target["blood-amount"].value
+        bloodAmt.textContent = parseInt(bloodAmt.textContent) + parseInt(newDrops)
+        // console.log(newDrops)
+        e.target.reset
+    })
